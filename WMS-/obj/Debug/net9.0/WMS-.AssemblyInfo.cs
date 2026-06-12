93a1f4d0881ee3e9bd92fec0b151702295c972e5
Trần Quang Khải <khaitranquang246810@gmail.com>
2026-06-12 12:34:44 +0700

    feat: cấu hình kết nối database

--- WMS-/obj/Debug/net9.0/WMS-.AssemblyInfo.cs
+++ WMS-/obj/Debug/net9.0/WMS-.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WMS-")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81e9998bd9bbf6336c97ca74df671326dbd8b1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd1400789332fa38d9785a793b537890965eb78c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WMS-")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WMS-")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
